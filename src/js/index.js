c921c0eb9e5fbc967060309c34b8ed664dfc34a3
Marsel Gray <marselgray@gmail.com>
2019-02-04 10:08:22 -0500

    Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
+import Search from './models/search.js';
 
-async function getResults(query){
-    const key = '269fb249d36c1e29f6a2c032c838b610';
-    try{
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        //console.log(res);
-        console.log(recipes); 
-    } catch(error) {
-        console.log(error)
-    }
-}
-getResults('tomato pasta');
+const search = new Search('pizza');
+console.log(search)
+search.getResults();
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -1,1 +1,17 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    async getResults(){
+        const key = '269fb249d36c1e29f6a2c032c838b610';
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result); 
+        } catch(error) {
+            console.log(error)
+        }
+    }
+}
