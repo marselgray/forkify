9d206356c0dc08c3848a6e267da17e02db99e3ea
Marsel Gray <marselgray@gmail.com>
2019-02-04 09:34:36 -0500

    API: connection and test

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import num from './test.js';
-const x = 25;
-console.log(`I imported ${num} from another Variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query){
+    const key = '269fb249d36c1e29f6a2c032c838b610';
+    try{
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        //console.log(res);
+        console.log(recipes); 
+    } catch(error) {
+        console.log(error)
+    }
+}
+getResults('tomato pasta');
